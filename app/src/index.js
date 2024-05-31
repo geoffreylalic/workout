6247948bbd0e8dc6eb3df915dccca33eec3206d2
Geoffrey <geoffrey0151@gmail.com>
2024-05-31 15:56:18 +0200

    feat: added home page

--- app/src/index.js
+++ app/src/index.js
@@ -8,7 +8,7 @@ import Login from "./pages/Login";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home/>,
   },
   {
     path: "/login",
