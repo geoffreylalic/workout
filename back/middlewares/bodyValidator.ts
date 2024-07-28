a570063ee95802b5cb09f5e5e5fe4b6f18e6d444
Geoffrey <geoffrey0151@gmail.com>
2024-07-28 12:51:48 +0200

    add request body validator using zod

---
+++ back/middlewares/bodyValidator.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodSchema } from "zod";
+
+export const bodyValidator =
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+    } catch (error) {
+      res.status(422).send(error);
+      return;
+    }
+    next();
+  };
