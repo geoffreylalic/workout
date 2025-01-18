63ddd58ea111b68965f2920297706a9c99c07c53
Geoffrey <geoffrey0151@gmail.com>
2025-01-18 23:51:17 +0100

    fix: rename

--- app/src/components/SideBar.js
+++ app/src/components/SideBar.js
@@ -146,7 +146,7 @@ const SideBar = () => {
                     </svg>
 
                     <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
-                      Accou,t
+                      Account
                     </span>
                   </a>
                 </li>
