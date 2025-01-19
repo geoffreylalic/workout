28d214e2837697ffb6888249d2562bb0fb3983d6
Geoffrey <geoffrey0151@gmail.com>
2025-01-19 14:31:37 +0100

    styles: padding table

--- app/src/pages/workouts/Workouts.js
+++ app/src/pages/workouts/Workouts.js
@@ -15,24 +15,27 @@ const Workouts = () => {
   if (data) {
     return (
       <div className="pl-5 pt-5">
-        <div className="justify-between grid grid-cols-5 gap-10">
+        <div className="flex justify-between px-3">
+          <div>workouts</div>
           <Button
             type="button"
             className="ml-6 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             onClick={setOpen}
           >
-            Add workout pp
+            Add workout
           </Button>
+          {/* <div className="justify-between grid grid-cols-5 gap-10"></div> */}
+          {/* <Calendar addName="add workout  " onClickAdd={handleAdd} /> */}
         </div>
-        {/* <Calendar addName="add workout  " onClickAdd={handleAdd} /> */}
-        <Table
-          data={data}
-          attributes={["name", "createdAt", "id"]}
-          headers={["name", "Date creation", "id", "action"]}
-          view="id"
-        />
-
-        <CreateWorkoutDialog open={open} setOpen={setOpen} />
+        <div className="pt-5">
+          <Table
+            data={data}
+            attributes={["name", "createdAt", "id"]}
+            headers={["name", "Date creation", "id", "action"]}
+            view="id"
+          />
+          <CreateWorkoutDialog open={open} setOpen={setOpen} />
+        </div>
       </div>
     );
   }
