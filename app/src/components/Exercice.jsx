affc301398aa674add6e494d9bbeabd81d923b74
Geoffrey <geoffrey0151@gmail.com>
2025-05-31 12:01:27 +0200

    refactor(exercice): removed useless logic

--- app/src/components/Exercice.jsx
+++ app/src/components/Exercice.jsx
@@ -25,44 +25,40 @@ const Exercice = ({ workoutId, exercice }) => {
 
   return (
     <div className="m-5">
-      {exercice && (
-        <div>
-          <div className="flex justify-between">
-            <h2 className="text-lg font-semibold mb-2">{exercice.name}</h2>
-            <Button onClick={() => mutationExercice.mutate(exercice.id)}>
-              Supprimer
-            </Button>
-          </div>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Repetitions</TableHead>
-                <TableHead>Poids</TableHead>
-                <TableHead>Repos</TableHead>
-                <TableHead>Actions</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {exercice &&
-                exercice?.sets?.length > 0 &&
-                exercice?.sets?.map((set, key) => (
-                  <TableRow key={key}>
-                    <Set set={set} workoutId={workoutId} />
-                  </TableRow>
-                ))}
-              <CreateSet workoutId={workoutId} exerciceId={exercice.id} />
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell>Total repetitions{} </TableCell>
-                <TableCell>Total poids </TableCell>
-                <TableCell>Total repos {} </TableCell>
-                <TableCell>Total tonage</TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
+        <div className="flex justify-between">
+          <h2 className="text-lg font-semibold mb-2">{exercice.name}</h2>
+          <Button onClick={() => mutationExercice.mutate(exercice.id)}>
+            Supprimer
+          </Button>
         </div>
-      )}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Repetitions</TableHead>
+              <TableHead>Poids</TableHead>
+              <TableHead>Repos</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {exercice &&
+              exercice?.sets?.length > 0 &&
+              exercice?.sets?.map((set, key) => (
+                <TableRow key={key}>
+                  <Set set={set} workoutId={workoutId} />
+                </TableRow>
+              ))}
+            <CreateSet workoutId={workoutId} exerciceId={exercice.id} />
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell>Total repetitions{} </TableCell>
+              <TableCell>Total poids </TableCell>
+              <TableCell>Total repos {} </TableCell>
+              <TableCell>Total tonage</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
     </div>
   );
 };
