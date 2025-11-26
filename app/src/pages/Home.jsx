8d7d79d79d3b4c1dc5011ae863b12c7753239963
Geoffrey <geoffrey0151@gmail.com>
2025-11-26 19:57:53 +0100

    style(dashboard): added date range picker

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,13 +2,38 @@ import { Button, Typography } from "@material-tailwind/react";
 import { useNavigate, useSearchParams } from "react-router";
 import Workouts from "../components/Workouts";
 import { VolumeChart } from "@/components/VolumeChart";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronDownIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { DatePicker } from "@/components/DatePicker";
+import { useState } from "react";
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [date, setDate] = useState(null);
   return (
     <div className="m-5">
       <div className="flex justify-between px-10 py-8">
-        <div className="">
-          <Typography className="text-2xl font-bold">Workouts</Typography>
+        <div className="flex gap-5">
+          <Typography className="text-2xl font-bold md-5">
+            Suivi d’entraînement
+          </Typography>
+          <DatePicker
+            startDate={startDate}
+            setStartDate={setStartDate}
+            endDate={endDate}
+            setEndDate={setEndDate}
+            onChange={(range) => {
+              setStartDate(range.from);
+              setEndDate(range.to);
+            }}
+            mode="range"
+          />
         </div>
         <Button
           onClick={() => {
