aff96a9c2c0d775c8c643c720c6a8c312237089f
Geoffrey <geoffrey0151@gmail.com>
2025-11-26 16:43:12 +0100

    feat(dashboard): added dynamic rendering of rest, weight and repetitions

--- app/src/components/VolumeChart.jsx
+++ app/src/components/VolumeChart.jsx
@@ -12,24 +12,55 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getVolume } from "@/queries/dashboard";
-
-export const description = "An interactive line chart";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Command, CommandGroup, CommandItem, CommandList } from "./ui/command";
+import { Check } from "lucide-react";
+import { Button } from "./ui/button";
 
 const chartConfig = {
-  views: {
-    label: "Volume",
-  },
-  totalWeight: {
-    label: "Weight",
+  weight: {
+    label: "Charge totale (kg)",
     color: "var(--chart-1)",
+  },
+  repetitions: {
+    label: "Nombre de répétitions",
+    color: "var(--chart-2)",
+  },
+  rest: {
+    label: "temps de repos (s)",
+    color: "var(--chart-3)",
   },
 };
 
 export const VolumeChart = () => {
-  const [activeChart, setActiveChart] = useState("totalWeight");
+  const [activeChart, setActiveChart] = useState("weight");
+  const [charts, setCharts] = useState(["weight", "repetitions", "rest"]);
+  const options = [
+    { value: "weight", label: "Weight" },
+    { value: "repetitions", label: "Repetitions" },
+    { value: "rest", label: "Rest" },
+  ];
+
+  const labelByValue = Object.fromEntries(
+    options.map((o) => [o.value, o.label])
+  );
+
+  const toggle = (v) => {
+    setCharts((prev) =>
+      prev.includes(v) ? prev.filter((x) => x !== v) : [...prev, v]
+    );
+  };
+
   const { data, isLoading, isError } = useQuery({
     queryFn: getVolume,
     queryKey: ["dashboard"],
@@ -37,43 +68,53 @@ export const VolumeChart = () => {
 
   if (isLoading) return <div>Loading ...</div>;
   if (isError) return <div>Error</div>;
-
-  const total = useMemo(
-    () => ({
-      totalWeight: data.reduce((acc, curr) => acc + curr.totalWeight, 0),
-    }),
-    []
-  );
-
   return (
     <Card className="py-4 sm:py-0">
-      <CardHeader className="flex flex-col items-stretch border-b !p-0 sm:flex-row">
-        <div className="flex flex-1 flex-col justify-center gap-1 px-6 pb-3 sm:pb-0">
-          <CardTitle>Line Chart - Interactive</CardTitle>
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <div className="grid flex-1 gap-1">
+          <CardTitle>Volume d'entrainement</CardTitle>
           <CardDescription>
             Showing total visitors for the last 3 months
           </CardDescription>
         </div>
-        <div className="flex">
-          {["totalWeight"].map((key) => {
-            const chart = key;
-            return (
-              <button
-                key={chart}
-                data-active={activeChart === chart}
-                className="data-[active=true]:bg-muted/50 flex flex-1 flex-col justify-center gap-1 border-t px-6 py-4 text-left even:border-l sm:border-t-0 sm:border-l sm:px-8 sm:py-6"
-                onClick={() => setActiveChart(chart)}
-              >
-                <span className="text-muted-foreground text-xs">
-                  {chartConfig[chart].label}
-                </span>
-                <span className="text-lg leading-none font-bold sm:text-3xl">
-                  {total[key].toLocaleString()}
-                </span>
-              </button>
-            );
-          })}
-        </div>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button className="w-[180px] p-2 border rounded-lg flex justify-between">
+              {charts.length === 0
+                ? "Select metrics"
+                : charts
+                    .map((c) => {
+                      return labelByValue[c];
+                    })
+
+                    .join(", ")}
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="p-0 w-[200px]">
+            <Command>
+              <CommandList>
+                <CommandGroup>
+                  {options.map((opt) => (
+                    <CommandItem
+                      key={opt.value}
+                      onSelect={() => toggle(opt.value)}
+                    >
+                      <Check
+                        className={
+                          charts.includes(opt.value)
+                            ? "mr-2 h-4 w-4 opacity-100"
+                            : "mr-2 h-4 w-4 opacity-0"
+                        }
+                      />
+                      {opt.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </CardHeader>
       <CardContent className="px-2 sm:p-6">
         <ChartContainer
@@ -107,7 +148,6 @@ export const VolumeChart = () => {
               content={
                 <ChartTooltipContent
                   className="w-[150px]"
-                  nameKey="views"
                   labelFormatter={(value) => {
                     return new Date(value).toLocaleDateString("fr-FR", {
                       month: "short",
@@ -118,13 +158,15 @@ export const VolumeChart = () => {
                 />
               }
             />
-            <Line
-              dataKey={activeChart}
-              type="monotone"
-              stroke={`var(--color-${activeChart})`}
-              strokeWidth={2}
-              dot={false}
-            />
+            {charts.map((c) => (
+              <Line
+                dataKey={c}
+                type="monotone"
+                stroke={`var(--color-${c})`}
+                strokeWidth={2}
+                dot={false}
+              />
+            ))}
           </LineChart>
         </ChartContainer>
       </CardContent>
